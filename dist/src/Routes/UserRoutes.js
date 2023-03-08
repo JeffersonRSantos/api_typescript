61575cb2d4f68d70cb37ca426de46f47e4058492
Jefferson Santos <61438281+JeffersonRSantos@users.noreply.github.com>
2023-03-08 15:53:17 -0300

    Delete dist/src directory

--- dist/src/Routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoutes = void 0;
-const express_1 = require("express");
-const userRoutes = (0, express_1.Router)();
-exports.userRoutes = userRoutes;
-userRoutes.get('/', (req, res, next) => {
-    return res.status(200).send("Olá, seja bem vindo a aplicação 'TypeScript com SOLID' ");
-});
